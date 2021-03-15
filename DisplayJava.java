3e6849c15db4cac9881bbf5e83e8e1f00a4ca46f
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-15 22:59:50 +0530

    Create DisplayJava.java

---
+++ DisplayJava.java
@@ -0,0 +1,5 @@
+public class DisplayJava {
+  public static main void(Stirng[] args) {
+    System.out.println("Dear Linguist, please use this .java file to detect 'Java' and to show it on my repository. \n\t Thank you, Ashish. ");  
+  }
+}
