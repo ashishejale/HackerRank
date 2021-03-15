8c258c23261e1125e14088b595fd7c5986a92189
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-16 00:48:44 +0530

    Delete DisplayJava.java

--- DisplayJava.java
+++
@@ -1,5 +0,0 @@
-public class DisplayJava {
-  public static main void(Stirng[] args) {
-    System.out.println("Dear Linguist, please use this .java file to detect 'Java' and to show it on my repository. \n\t Thank you, Ashish. ");  
-  }
-}
