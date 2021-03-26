7c692f21426112491d8dfd5547332c9ca05aa4ec
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-26 13:46:02 +0530

    Update day6.java

--- 30DaysOfCode/day6.java
+++ 30DaysOfCode/day6.java
@@ -7,23 +7,24 @@ public class Solution {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
-        scanner.nextLine();
+        scanner.nextLine(); // to empty buffer after accepting an int 
         
-        for(int i = 0; i < t; i++){
-            String s = scanner.nextLine();
+        for(int i = 0; i < t; i++){ //to count input strings
+            String s = scanner.nextLine(); // accept string
             for(int j = 0; j < s.length(); j++) {
                 if(j % 2 == 0) { //even 
-                    System.out.print(s.charAt(j));
+                    System.out.print(s.charAt(j)); // print even char 
                 }
             }
-                System.out.print(" ");
+            
+            System.out.print(" "); // add space
                 
             for(int j = 0; j < s.length(); j++) {
                 if(j % 2 != 0) { //odd
-                    System.out.print(s.charAt(j));
+                    System.out.print(s.charAt(j)); //print odd char
                 }
             }
-            System.out.println();      
+            System.out.println();    //go to next line and loop to get next String 
         }
         scanner.close();
     }
